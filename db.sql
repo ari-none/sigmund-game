bce245ef016a71d9e5602849a957f49a2ff80060
ari-none <enzohochin@gmail.com>
2026-02-12 14:10:22 +0100

    J'avais oublié de mettre une contrainte AUTO_INCREMENT sur les clés primaires

--- db.sql
+++ db.sql
@@ -16,14 +16,14 @@ USE Simon;
 -- Table: UTILISATEUR --
 -- ------------------ --
 CREATE TABLE UTILISATEUR (
-        id INT NOT NULL,
-        pseudo VARCHAR(50) NOT NULL,
-        password CHAR(64) NOT NULL,
-        email VARCHAR(50),
-        CONSTRAINT UTILISATEUR_PK PRIMARY KEY (id),
-        CONSTRAINT id_UNQ UNIQUE (id),
-        CONSTRAINT pseudo_UNQ UNIQUE (pseudo),
-        CONSTRAINT email_UNQ UNIQUE (email)
+                             id INT NOT NULL AUTO_INCREMENT,
+                             pseudo VARCHAR(50) NOT NULL,
+                             password CHAR(64) NOT NULL,
+                             email VARCHAR(50),
+                             CONSTRAINT UTILISATEUR_PK PRIMARY KEY (id),
+                             CONSTRAINT id_UNQ UNIQUE (id),
+                             CONSTRAINT pseudo_UNQ UNIQUE (pseudo),
+                             CONSTRAINT email_UNQ UNIQUE (email)
 )ENGINE=InnoDB;
 
 
@@ -31,12 +31,12 @@ CREATE TABLE UTILISATEUR (
 -- Table: PARTIE --
 -- ------------- --
 CREATE TABLE PARTIE (
-        id INT NOT NULL,
-        sequence VARCHAR(255) NOT NULL,
-        temps DECIMAL(10,3) NOT NULL,
-        id_utilisateur INT NOT NULL,
-        CONSTRAINT PARTIE_PK PRIMARY KEY (id),
-        CONSTRAINT id_UNQ UNIQUE (id),
-        CONSTRAINT id_utilisateur_UNQ UNIQUE (id_utilisateur),
-        CONSTRAINT PARTIE_id_utilisateur_FK FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR (id)
+                        id INT NOT NULL AUTO_INCREMENT,
+                        sequence VARCHAR(255) NOT NULL,
+                        temps DECIMAL(10,3) NOT NULL,
+                        id_utilisateur INT NOT NULL,
+                        CONSTRAINT PARTIE_PK PRIMARY KEY (id),
+                        CONSTRAINT id_UNQ UNIQUE (id),
+                        CONSTRAINT id_utilisateur_UNQ UNIQUE (id_utilisateur),
+                        CONSTRAINT PARTIE_id_utilisateur_FK FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR (id)
 )ENGINE=InnoDB;
